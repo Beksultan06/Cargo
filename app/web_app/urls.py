920892a66d10d6edeec51a84f3f94182dc1203f9
didiotghima <nurlanuuulubeksultan@gmail.com>
2025-01-29 22:05:15 +0600

    add chat_id

--- app/web_app/urls.py
+++ app/web_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from app.web_app.views import register, cargopart
+from app.web_app.views import register, cargopart, warehouse
 
 urlpatterns = [
     path("", register, name='register'),
-    path("cargopart/", cargopart, name='cargopart')
+    path("cargopart/", cargopart, name='cargopart'),
+    path("warehouse/", warehouse, name='warehouse')
 ]
--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-SITE_BASE_URL=" https://4730-158-181-248-104.ngrok-free.app"
+SITE_BASE_URL="https://4730-158-181-248-104.ngrok-free.app"
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
