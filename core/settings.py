7b46f6776bea0ce90b17c889ec859664b751c9d6
isko18 <toksonbaevislam2004@gmail.com>
2025-01-31 00:03:19 +0600

    fix web

--- core/settings.py
+++ core/settings.py
@@ -22,15 +22,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-SITE_BASE_URL="https://4730-158-181-248-104.ngrok-free.app"
+SITE_BASE_URL="https://76eb-158-181-248-104.ngrok-free.app"
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
-
+ALLOWED_HOSTS = [
+    "76eb-158-181-248-104.ngrok-free.app",
+    "127.0.0.1",
+    "localhost"
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -47,7 +50,9 @@ INSTALLED_APPS = [
     "app.telegram",
 ]
 
-CSRF_TRUSTED_ORIGINS = ["https://4730-158-181-248-104.ngrok-free.app"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://a81c-158-181-248-104.ngrok-free.app"
+]
 
 LOGIN_URL = '/cargopart/'
 
